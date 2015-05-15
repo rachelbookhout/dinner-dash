058c335d4489aabea0222ce28ba88f01bb2be69b
rachelbookhout <rbookhou@gmail.com>
2015-05-15 19:25:02 -0400

    added more association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
-has_many :orders, :through :ItemsinOrders
-has_many :carts, :through :ItemsinCart
+  has_many :orders, :through :ItemsinOrders
+  has_many :carts, :through :ItemsinCart
 end
