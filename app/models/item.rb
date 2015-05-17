81e94fba406225ee3c2cbf48f6b29a54ddfddf0d
rachelbookhout <rbookhou@gmail.com>
2015-05-17 08:48:32 -0400

    added validations on items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ActiveRecord::Base
   has_many :orders
   has_many :orders, :through :ItemsinOrders
   has_many :carts, :through :ItemsinCart
+  belongs_to :category
+
+  validates :tite, presence:true
+  validates :price, presence:true
+  validates :description, presence:true
+  validates :title, uniqueness:true
+  validates :title, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Title only allows letters" }
+    validates :description, format: { with: /\A[a-zA-Z]+\z/,
+    message: "Description only allows letters" }
+  validates :price, numericality: true
+  validates :price, numericality: { greater_than: 0 }
 end
